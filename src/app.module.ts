1625afd93acd8d9dbcfc77f86ef8cec7a6ca2eff
bsc-com-22-20 <bsc-com-22-20@unima.ac.mw>
2023-05-17 01:59:55 +0300

    another commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
 import { MenusModule } from './menus/menus.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { CustomersModule } from 'src/customers/customers.module';
 import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './local.startegy';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
   imports: [CustomersModule, PassportModule],
