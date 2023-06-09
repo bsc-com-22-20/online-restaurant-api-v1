6369e6a1f71b43fa7912ff43151b72f465b9d070
bsc-com-22-20 <bsc-com-22-20@unima.ac.mw>
2023-06-09 16:05:04 +0300

    updated

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   app.use(helmet());
 
