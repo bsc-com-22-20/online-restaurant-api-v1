7c81b47d00e875b36ed1bb4c902236b3396732ca
bsc-com-22-20 <bsc-com-22-20@unima.ac.mw>
2023-06-09 17:03:11 +0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // const app = await NestFactory.create(AppModule);
   // app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   app.use(helmet());
